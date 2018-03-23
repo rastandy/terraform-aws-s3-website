db3006e8148eb6ff5f92901c42a42454cb35f130
Andrea Russo <andrea.russo@cmcc.it>
2018-03-23 10:29:22 +0100

    Added the s3_bucket_website_endpoint output var.

--- output.tf
+++ output.tf
@@ -6,6 +6,10 @@ output "s3_bucket_domain_name" {
   value = "${aws_s3_bucket.default.bucket_domain_name}"
 }
 
+output "s3_bucket_website_endpoint" {
+  value = "${aws_s3_bucket.default.website_endpoint}"
+}
+
 output "hostname" {
   value = "${var.hostname}"
 }
